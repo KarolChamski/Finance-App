29a2f1424a1cec43451ff254619bc0df4138e12e
Karol <karol.chamski@gmail.com>
2021-12-28 22:17:23 +0100

    Set Vue Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
 import store from './store'
-createApp(App).use(store).mount('#app')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/home', component: App } 
+    ]
+  });
+
+const app = createApp(App)
+
+app.use(router);
+app.use(store)
+app.mount('#app');
+
+// createApp(App).use(store).mount('#app')
